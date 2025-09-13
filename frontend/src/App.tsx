da10e833470e987e967f78967c6a80190f1a3846
Vinayak Arora <vinayakarora7461@gmail.com>
2025-09-13 11:58:23 +0530

    feat: initialize frontend with Vite, React, and Tailwind CSS ependencies and scripts.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-center text-blue-600">
+        Scheduler Frontend (Vite Edition!)
+      </h1>
+    </div>
+  );
+}
+
+export default App;
